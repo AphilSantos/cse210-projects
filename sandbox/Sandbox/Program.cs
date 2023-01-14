135c9a85907297e537729682f35cc8a825cc407b
Itsadesign2022 <aa.design1921@gmail.com>
2023-01-14 16:36:08 +0800

    performed prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+
+        Console.WriteLine("What is your name?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
     }
 }
