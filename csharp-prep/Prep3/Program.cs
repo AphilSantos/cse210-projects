12a55b4eae60353176ed695de65224e039c1f9a4
Itsadesign2022 <aa.design1921@gmail.com>
2023-01-18 16:30:40 +0800

    random number guess completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+
+        int num = random.Next(1, 11);
+
+        Console.WriteLine(num);
+
+        Console.WriteLine("Guess the magic number");
+
+        int userInput = -1;
+
+        while (userInput != num)
+        {
+            Console.Write("Please guess a number between 1 through 10: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (num > userInput)
+            {
+                Console.WriteLine("Try again");
+                Console.WriteLine("Higher");
+            }
+
+            else if (num < userInput)
+            {
+                Console.WriteLine("Try again");
+                Console.WriteLine("Lower");
+            }
+            
+            else if (num == userInput)
+            
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
