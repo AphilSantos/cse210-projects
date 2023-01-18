31a0bb6bc3b10aee16f16565b4d3cc96365aa8ab
Itsadesign2022 <aa.design1921@gmail.com>
2023-01-18 15:56:02 +0800

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int gradeOfUser = int.Parse(grade);
+
+        if (gradeOfUser >= 90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if (gradeOfUser >= 80)
+        {
+            Console.WriteLine("B");
+        }
+
+        else if (gradeOfUser >= 70)
+        {
+            Console.WriteLine("C");
+        }
+
+        else if (gradeOfUser >= 60)
+        {
+            Console.WriteLine("D");
+        }
+
+        else if (gradeOfUser <= 60)
+        {
+            Console.WriteLine("F");
+        }
+
+        if (gradeOfUser >= 71)
+        {
+            Console.Write("Congratulations, you passed! ");
+        }
+
+        else 
+        {
+            Console.Write("You failed. Try again, YOU CAN DO IT!!");
+        }
+
     }
 }
