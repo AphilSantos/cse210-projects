112879dc76b79c765c043d6e8b757e38af5cde40
Itsadesign2022 <aa.design1921@gmail.com>
2023-01-18 17:24:10 +0800

    functions completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+
+        string DisplayName = PromptUserName();
+
+        int DisplayNumber = PromptUserNumber();
+
+        int DisplaySquared = PromptUserNumberToSquare(DisplayNumber);
+
+        Combine(DisplayName, DisplaySquared);
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your number? ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        return userNumber;
+    }
+
+    static int PromptUserNumberToSquare(int userNumber)
+    {
+        int square = userNumber * userNumber;
+
+        return square;
+    }
+
+    static void Combine(string name, int square)
+    {
+        Console.WriteLine($"Your name is {name} and your squared number is {square}.");
     }
 }
